f0427038664db9ad7b58a1e6c08a27036d572d19
arjuncvinod <arjuncvinod007@gnail.com>
2023-08-15 19:43:55 +0530

    updated

--- src/token_assets/src/index.jsx
+++ src/token_assets/src/index.jsx
@@ -1,11 +1,32 @@
-import ReactDOM from 'react-dom'
-import React from 'react'
+import ReactDOM from "react-dom";
+import React from "react";
 import App from "./components/App";
+import { AuthClient } from "@dfinity/auth-client";
+import { Principal } from "@dfinity/principal";
 
-const init = async () => { 
-  ReactDOM.render(<App />, document.getElementById("root"));
+const init = async () => {
+  const authClient = await AuthClient.create();
+
+  if (await authClient.isAuthenticated()) {
+    handleAuthenticated(authClient);
+  } else {
+    await authClient.login({
+      identityProvider: "https://identity.ic0.app/#authorize",
+      onSuccess: () => {
+        handleAuthenticated(authClient);
+      },
+    });
+  }
+};
+
+async function handleAuthenticated(authClient) {
+  const identity = await authClient.getIdentity();
+  const userPrincipal = identity._principal.toString();
+  console.log(userPrincipal);
+  ReactDOM.render(
+    <App loggedInPrincipal={userPrincipal} />,
+    document.getElementById("root")
+  );
 }
 
 init();
-
-
