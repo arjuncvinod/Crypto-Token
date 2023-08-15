7853e547f40709f1ff9a65e372b43a26abd672d0
arjuncvinod <arjuncvinod007@gnail.com>
2023-08-15 19:50:25 +0530

    updated

--- src/token_assets/src/components/Transfer.jsx
+++ src/token_assets/src/components/Transfer.jsx
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import { Principal } from '@dfinity/principal';
+import { canisterId, createActor } from "../../../declarations/token";
+import { AuthClient } from "@dfinity/auth-client";
 
 function Transfer() {
+
+  const [recipientId, setId] = useState("");
+  const [amount, setAmount] = useState("");
+  const [isHidden, setHidden] = useState(true);
+  const [feedback, setFeedback] = useState("");
+  const [isDisabled, setDisable] = useState(false);
   
   async function handleClick() {
-    
+    setHidden(true);
+    setDisable(true);
+    const recipient = Principal.fromText(recipientId);
+    const amountToTransfer = Number(amount);
+
+    const authClient = await AuthClient.create();
+    const identity = await authClient.getIdentity();
+    const authenticatedCanister = createActor(canisterId, {
+      agentOptions: {
+        identity,
+      },
+    });
+
+    const result = await authenticatedCanister.transfer(recipient, amountToTransfer);
+    setFeedback(result);
+    setHidden(false);
+    setDisable(false);
   }
 
   return (
@@ -16,6 +41,8 @@ function Transfer() {
               <input
                 type="text"
                 id="transfer-to-id"
+                value={recipientId}
+                onChange={(e) => setId(e.target.value)}
               />
             </li>
           </ul>
@@ -27,15 +54,22 @@ function Transfer() {
               <input
                 type="number"
                 id="amount"
+                value={amount}
+                onChange={(e) => setAmount(e.target.value)}
               />
             </li>
           </ul>
         </fieldset>
         <p className="trade-buttons">
-          <button id="btn-transfer" onClick={handleClick} >
+          <button 
+          id="btn-transfer" 
+          onClick={handleClick} 
+          disabled={isDisabled}
+          >
             Transfer
           </button>
         </p>
+        <p hidden={isHidden}>{feedback}</p>
       </div>
     </div>
   );
